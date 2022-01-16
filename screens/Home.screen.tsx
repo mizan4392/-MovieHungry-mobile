f635339a149ce90668a3a7a5bb52e532787cbbaa
mizan4392 <md.mizan4392@gmail.com>
2022-01-17 01:59:49 +0600

    movie flat-list

--- screens/Home.screen.tsx
+++ screens/Home.screen.tsx
@@ -1,41 +1,57 @@
 import React, {useEffect, useState} from 'react';
-import {View} from 'react-native';
-import {getUpcomingMovies} from '../requests/get/movies';
+import {Dimensions, StyleSheet, View} from 'react-native';
+import {getPopularMovies, getUpcomingMovies} from '../requests/get/movies';
 import {} from '../requests/get/movies';
 import {MovieI} from '../requests/get/movies.interface';
 //@ts-ignore
 import {SliderBox} from 'react-native-image-slider-box';
 import {IMAGE_BASE} from '../utils/http';
+import List from '../components/List.component';
 export const Home = () => {
-  const [movies, setMovies] = useState<Array<MovieI>>([]);
+  const [upComingMovies, setUpComingMovies] = useState<Array<MovieI>>([]);
+  const [popularMovies, setPopularMovies] = useState<Array<MovieI>>([]);
   const [movieImages, setMovieImages] = useState<Array<string>>([]);
-
+  const dimenssion = Dimensions.get('screen');
   useEffect(() => {
     getUpcomingMovies().then(moviesres => {
       const images: Array<string> = [];
       moviesres.results?.forEach((movie: MovieI) => {
         images.push(`${IMAGE_BASE}${movie.poster_path}`);
       });
-      setMovieImages(images);
 
-      setMovies(moviesres.results);
+      setMovieImages(images);
+      setUpComingMovies(moviesres.results);
+    });
+
+    getPopularMovies().then(popular => {
+      setPopularMovies(popular.results);
     });
   }, []);
+
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <SliderBox
-        // images={[
-        //   'https://image.tmdb.org/t/p/w500/nk26r1JoKEXpptobBRBFRdq22lV.jpg',
-        //   'https://image.tmdb.org/t/p/w500/7GcqdBKaMM9BWXWN07BirBMkcBF.jpg',
-        //   'https://image.tmdb.org/t/p/w500/30SLnoKmYNyRPkKCYowsrGLRnJA.jpg',
-        //   'https://image.tmdb.org/t/p/w500/gDaxYkYNbHuM2VlUazbcpnFZB6d.jpg',
-        //   'https://image.tmdb.org/t/p/w500/pj0EPtI1mEwqjR34RQJdnbns0am.jpg',
-        //   'https://image.tmdb.org/t/p/w500/ruHDFumJfW7F2vEqTZEQQ9xT7CA.jpg',
-        //   'https://image.tmdb.org/t/p/w500/odVv1sqVs0KxBXiA8bhIBlPgalx.jpg',
-        // ]}
-        images={movieImages}
-        sliderBoxHeight={600}
-      />
-    </View>
+    <React.Fragment>
+      <View style={styles.sliderContainer}>
+        <SliderBox
+          images={movieImages}
+          circleLoop={true}
+          sliderBoxHeight={dimenssion.height / 1.5}
+          autoPlay={true}
+          dotStyle={styles.sliderOdtStyle}
+        />
+      </View>
+      <View style={styles.sliderContainer}>
+        <List movies={popularMovies} title="Popular Movies" />
+      </View>
+    </React.Fragment>
   );
 };
+const styles = StyleSheet.create({
+  sliderContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  sliderOdtStyle: {
+    height: 0,
+  },
+});
